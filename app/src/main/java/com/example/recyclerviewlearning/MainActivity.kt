96cc3c3bd891652da11405f9950529a1666f6c83
KO <jy32m0@gmail.com>
2022-05-28 05:56:04 -1000

    Initial Commit after completion.

---
+++ app/src/main/java/com/example/recyclerviewlearning/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.recyclerviewlearning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        layoutManager = LinearLayoutManager(this)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewDa)
+        recyclerView.layoutManager = layoutManager
+
+        adapter = RecyclerAdapter()
+        recyclerView.adapter = adapter
+    }
+}
---
+++ app/src/main/java/com/example/recyclerviewlearning/RecyclerAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.recyclerviewlearning
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    private var titles = arrayOf("Chapter One", "Chapter Two", "Chapter Three", "Chapter Four",
+        "Chapter Five", "Chapter Six", "Chapter Seven", "Chapter Eight")
+    private var details = arrayOf("Item one details", "Item two details", "Item three details",
+        "Item four details", "Item five details", "Item six details", "Item seven details", "Item eight details")
+    private var images = intArrayOf(R.drawable.uspslogo, R.drawable.uspslogo, R.drawable.uspslogo,
+        R.drawable.uspslogo, R.drawable.uspslogo, R.drawable.uspslogo, R.drawable.uspslogo, R.drawable.uspslogo)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
+        holder.itemTitle.text = titles[position]
+        holder.itemDetail.text = details[position]
+        holder.itemImage.setImageResource(images[position])
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemDetail: TextView
+
+        init {
+            itemImage = itemView.findViewById(R.id.item_image)
+            itemTitle = itemView.findViewById(R.id.item_title)
+            itemDetail = itemView.findViewById(R.id.item_detail)
+
+            itemView.setOnClickListener {
+                val position: Int = absoluteAdapterPosition
+
+                Toast.makeText(itemView.context, "You have clicked ${titles[position]}", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
